2d77df14a4c58f1703efea893ba5d5e7a1d00247
jingang <3839966@qq.com>
2017-09-08 18:29:37 +0800

    修改LogAspect

--- cjbase-biz/src/main/java/com/xcz/afcs/biz/aspect/LogAspect.java
+++ cjbase-biz/src/main/java/com/xcz/afcs/biz/aspect/LogAspect.java
@@ -3,7 +3,7 @@ package com.xcz.afcs.biz.aspect;
 import com.alibaba.fastjson.JSON;
 import com.xcz.afcs.api.param.BaseParameter;
 import lombok.Setter;
-import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.JoinPoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,7 +27,7 @@ public abstract class LogAspect {
         LOG.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> " + getClass().getName() + " loaded");
     }
     
-    public void beforeLog(ProceedingJoinPoint pjp) {
+    public void beforeLog(JoinPoint pjp) {
         startTime.set(System.currentTimeMillis());
         if (pjp.getArgs() != null) {
             Object param = pjp.getArgs()[0];
