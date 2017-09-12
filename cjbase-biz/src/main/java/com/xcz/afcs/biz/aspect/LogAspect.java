7e9cf438f0c24441ed0df1ab991976d4ac8378ab
mac <3839966@qq.com>
2017-09-12 23:38:29 +0800

    修改日志切面用环绕

--- cjbase-biz/src/main/java/com/xcz/afcs/biz/aspect/LogAspect.java
+++ cjbase-biz/src/main/java/com/xcz/afcs/biz/aspect/LogAspect.java
@@ -4,14 +4,15 @@ import com.alibaba.fastjson.JSON;
 import com.xcz.afcs.api.param.BaseParameter;
 import lombok.Setter;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
 
 public abstract class LogAspect {
 
     private static final Logger LOG = LoggerFactory.getLogger(LogAspect.class);
-
-    private ThreadLocal<Long> startTime = new ThreadLocal<Long>();
 
     @Setter
     private String from = "client";
@@ -27,20 +28,24 @@ public abstract class LogAspect {
         LOG.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> " + getClass().getName() + " loaded");
     }
     
-    public void beforeLog(JoinPoint pjp) {
-        startTime.set(System.currentTimeMillis());
-        if (pjp.getArgs() != null) {
-            Object param = pjp.getArgs()[0];
-            if (param instanceof BaseParameter) {
-                LOG.info(getInMark()+ " "+JSON.toJSONString(param));
+    public Object log(ProceedingJoinPoint pjp) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object result = null;
+        try {
+            if (pjp.getArgs() != null) {
+                Object param = pjp.getArgs()[0];
+                if (param instanceof BaseParameter) {
+                    LOG.info(getInMark() + " " + JSON.toJSONString(param));
+                }
+            } else {
+                LOG.info(getInMark() + " {}");
             }
-        }else{
-            LOG.info(getInMark() + " {}");
+            result = pjp.proceed();
+            return result;
         }
-    }
-
-    public void afterLog(Object result) {
-        LOG.info(getOutMark()+ " "+JSON.toJSONString(result)+" 处理时间:"+(System.currentTimeMillis()-startTime.get())+" 毫秒");
+        finally{
+            LOG.info(getOutMark()+ " "+JSON.toJSONString(result)+" 处理时间:"+(System.currentTimeMillis()-startTime)+" 毫秒");
+        }
     }
 
 
@@ -58,5 +63,4 @@ public abstract class LogAspect {
         return outMark;
     }
 
-    
 }
