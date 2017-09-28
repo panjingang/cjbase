45b06ee9ec9dac487fd6689215968e1b0fd44649
jingang <3839966@qq.com>
2017-09-28 09:13:28 +0800

    增加比较辅助类

---
+++ cjbase-util/src/main/java/com/xcz/afcs/util/CompareUtil.java
@@ -0,0 +1,17 @@
+package com.xcz.afcs.util;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by jingang on 2017/9/27.
+ */
+public class CompareUtil {
+
+    public static boolean compare(BigDecimal src, BigDecimal dst) {
+        if (src == null || dst == null) {
+            throw new IllegalArgumentException("比较的值不能为空");
+        }
+        return src.compareTo(dst) >= 0 ;
+    }
+
+}
